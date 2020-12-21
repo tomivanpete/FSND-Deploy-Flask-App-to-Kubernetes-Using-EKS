e419dac7f93e05664e0f928068e9321e87cab4ba
tomivanpete <tmimp916@gmail.com>
2020-12-20 19:17:04 -0800

    test CICD

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def auth():
 @APP.route('/contents', methods=['GET'])
 def decode_jwt():
     """
-    Check user token and return non-secret data
+    Check user token and return non-secret data.
     """
     if not 'Authorization' in request.headers:
         abort(401)
